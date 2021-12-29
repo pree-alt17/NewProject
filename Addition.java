e9660caa0690c7e2ffa64298115c831fc6e54d13
pree-alt17 <72858160+pree-alt17@users.noreply.github.com>
2021-12-29 17:31:57 +0530

    Addition.java
    addition of two integers

---
+++ Addition.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class AddNumbers {
+   
+    public static void main(String[] args) {
+        
+        int a,b, add;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        a = sc.nextInt();
+        
+        System.out.println("Enter Second Number: ");
+        b = sc.nextInt();
+        
+        sc.close();
+        add = num1 + num2;
+        System.out.println("Sum of these numbers: "+add);
+    }
+}
+    
